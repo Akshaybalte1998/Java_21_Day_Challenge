92113db43f7f75b051a82922186ffd08c1e2c423
Akshaybalte1998 <balteakshay1998@gmail.com>
2024-06-01 23:11:44 +0530

    adding codes in day 1

---
+++ Day1/Input.java
@@ -0,0 +1,29 @@
+package AsimpleProgram;
+
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//       //Scanner input = new Scanner(System.in);
+//		 Scanner input = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        int number = input.nextInt();
+//        System.out.println("You entered " + number);
+//
+//        // closing the scanner object
+//        input.close();
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter the number");
+		
+	
+		int num = input.nextInt();
+		 System.out.println("number = "+num);
+		 
+       input.close();
+        
+
+	}
+
+}
