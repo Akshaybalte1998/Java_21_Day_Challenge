0f52b910804f4a959c188ca9d41cf1fb1ca5c03b
Akshaybalte1998 <balteakshay1998@gmail.com>
2024-06-03 22:03:26 +0530

    adding data in day3

---
+++ Day3/arrayUtility/arrayUtilityMain.java
@@ -0,0 +1,22 @@
+package com.arrayUtility;
+
+
+
+public class arrayUtilityMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = new int[5];
+
+		
+		
+		arrayUtilityClass.AcceptArrayElement(arr);
+		arrayUtilityClass.DisplayArrayElement(arr);
+		
+		arrayUtilityClass.SumArrayElement(arr);
+	
+		
+	}
+
+}
