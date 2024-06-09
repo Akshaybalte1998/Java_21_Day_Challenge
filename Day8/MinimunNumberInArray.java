18ee644f2e19f57f0b5e59836741c6d5ba5500b9
Akshaybalte1998 <balteakshay1998@gmail.com>
2024-06-09 23:18:30 +0530

    adding new file

---
+++ Day8/MinimunNumberInArray.java
@@ -0,0 +1,40 @@
+package com.inputArray;
+
+import java.util.Scanner;
+
+public class MinimunNumberInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter the size of array");
+		int size =scan.nextInt();
+		   if (size <= 0) {
+	            System.out.println("Array size must be a positive integer.");
+	            return; // Exit the program
+	        }
+		int[] array=new int[size];
+		for(int i=0;i<array.length;i++)
+		{
+			System.out.println(" enter the "+i+" array elemnet");
+			array[i]=scan.nextInt();
+		}
+		System.out.println("array element in given order");
+		for(int i=0;i<array.length;i++)
+		{
+			System.out.println(array[i]);
+		}
+		
+		//finding minimun array 
+		int minimum=array[0];
+		for(int i =1;i<array.length;i++)
+		{
+			if(array[i]<minimum)
+			{
+				minimum=array[i];
+			}
+		}
+		System.out.println(" maximum number in given array is : "+minimum);
+	}
+
+}
